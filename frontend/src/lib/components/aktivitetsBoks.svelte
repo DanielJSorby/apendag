b4df360d96c1845c386e11df82b6c3ab2b519816
Simen Evenrud Blien <102034030+SimenBlien@users.noreply.github.com>
2025-11-12 13:30:01 +0100

    Add aktivitetsBoks component and kalender24 page
    Introduces a new Svelte component 'aktivitetsBoks' for displaying activity information and adds a new page at /kalender24 with basic structure for activities before and after lunch.

---
+++ frontend/src/lib/components/aktivitetsBoks.svelte
@@ -0,0 +1,24 @@
+<script>
+    interface Props {
+        title?: String
+        tidspunkt?: String
+        farge?: any
+    }
+    let {
+        title = ""
+        tidspunkt = ""
+        farge 
+    }
+</script>
+<div id="aktivitetBoks">
+    <div class="tekst">
+      <h1>{title}</h1>
+      <h3>{tidspunkt}</h3>
+    </div>
+</div>
+<style>
+    #aktivitetBoks {
+        padding: 5rem;
+        height: 10rem;
+    }
+</style>
---
+++ frontend/src/routes/kalender24/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    const response = await fetch("")
+</script>
+<div class="aktiviteter">
+    <div class="førlunsj"></div>
+    <div class="etterLunsj"></div>
+</div>
