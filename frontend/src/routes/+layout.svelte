9350445448c02a3d0505f4c9fe106e5e74afdccd
Johan Album Arntzen <100567482+JohanArntzen@users.noreply.github.com>
2025-11-17 12:21:16 +0100

    Endret favicon på link til elvebakken logoen

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import ElvebakkenLogo from '$lib/assets/ElvebakkenLogo.svg';
 	import Navbar from '$lib/components/navbar.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={ElvebakkenLogo} />
 </svelte:head>
 <Navbar />
 
